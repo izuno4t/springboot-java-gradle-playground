31d4e7fd41c7423e16499d0558bb00383a0f5fdc
tadashi izuno <tadashi.izuno@gmail.com>
2019-06-20 07:17:38 +0900

    update

--- gradle5-java/src/main/java/com/example/gradle/entity/Person.java
+++ gradle5-java/src/main/java/com/example/gradle/entity/Person.java
@@ -7,18 +7,30 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * 個人を表すエンティティです。
+ */
 @Entity
 @Table(name = "people")
 @Data
 public class Person {
 
+    /**
+     * ID
+     */
     @Id
     @Column(name="id")
     private int id;
 
+    /**
+     * 名前
+     */
     @Column(name="name")
     private String name;
 
+    /**
+     * 性別
+     */
     @Column(name="gender")
     private String gender;
 
--- gradle5-java/src/main/java/com/example/gradle/repository/PersonRepository.java
+++ gradle5-java/src/main/java/com/example/gradle/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.example.gradle.repository;
 import com.example.gradle.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Person エンティティのリポジトリです。
+ */
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 }
